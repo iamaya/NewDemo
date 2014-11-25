8c630d07279de93ce8e65c21745bcc6db923da93
iamaya <imaya.kumar@yahoo.com>
2014-11-24 20:02:12 -0500

    Next commit

---
+++ WebTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WebTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        [ExpectedException(typeof(NotImplementedException))]
+        public void TestMethod1()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
